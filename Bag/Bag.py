2c5cd5cbfe81c7248c4c5f0ac3bc980464b388ff
JaredMThompson <jaredproemail@gmail.com>
2026-09-25 11:06:38 -0600

    Move some stuff around

---
+++ Bag/Bag.py
@@ -0,0 +1,5 @@
+def main():
+    #call somthn
+
+if __name__ == '__main__':
+    main()
---
+++ Bag/Student.py
@@ -0,0 +1,8 @@
+class Bag():
+    def __init__(self):
+        pass
+    def insert(self, data):
+        #on dupe return false
+        return
+    def delete(self, data):
+        #return false if item does not exist in bag
